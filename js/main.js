210cc6f7611fbcea2f0e849bdf7762483c35dcc3
IlariaGalliano <ilaria.galliano7@gmail.com>
2020-10-16 16:29:06 +0200

    js-jsnacks-blocco-1
    Esercizi completi

--- js/main.js
+++ js/main.js
@@ -74,7 +74,7 @@ var nameFound = false;
 // Ciclo
 for (var i = 0; i < list.length; i++) {
   var item = list[i];
-if (name === item) {
+if (name.toLowerCase() === item.toLowerCase()){
   nameFound = true;
 }
 }
